1e55c385240bb8193b633e6fdd46a2948b9734c9
Sam Bybee <samuel.bybee@stu.glasgow.kyschools.us>
2025-12-01 08:53:09 -0600

    Initial Commit

---
+++ iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Student on 12/1/25.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
